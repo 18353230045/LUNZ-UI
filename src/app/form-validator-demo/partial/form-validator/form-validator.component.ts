0199909618a67ca3a80daa3ce0b49d8a7cea2314
leihg <13589299411@163.com>
2018-03-23 21:43:59 +0800

    Optimization of "form validator"

--- src/app/form-validator-demo/partial/form-validator/form-validator.component.ts
+++ src/app/form-validator-demo/partial/form-validator/form-validator.component.ts
@@ -12,7 +12,6 @@ const certainPassword = new FormControl('', CustomValidators.equalTo(password));
     styleUrls: ['./form-validator.component.scss']
 })
 
-
 export class FormValidatorComponent implements OnInit {
     form: FormGroup;
 
@@ -25,18 +24,18 @@ export class FormValidatorComponent implements OnInit {
             number: [null, [CustomValidators.number]],
             min: [null, [CustomValidators.number, CustomValidators.min(5)]],
             max: [null, [CustomValidators.number, CustomValidators.max(5)]],
-            rangeLength: [null, [CustomValidators.rangeLength([5, 9])]],
+            maxLength: [null, [Validators.maxLength(5)]],
+            minLength: [null, [Validators.minLength(5)]],
             date: [null, [CustomValidators.date]],
             minDate: [null, [CustomValidators.minDate('2018-03-07')]],
             maxDate: [null, [CustomValidators.maxDate('2018-03-07')]],
             phone: [null, [Validators.pattern(/^((0\d{2,3}-\d{7,8})|(1[3|4|5|7|8][0-9]\d{8}))$/)]],
             url: [null, [CustomValidators.url]],
             password: password,
-            equalTo: certainPassword,
+            equalTo: certainPassword
         });
-    }
+    };
 
     ngOnInit() {
-    }
-
+    };
 };
