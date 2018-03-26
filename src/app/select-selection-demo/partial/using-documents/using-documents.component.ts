c731b131f6fcfc49f0266d5b1590c14d320a839d
leihg <13589299411@163.com>
2018-03-26 09:28:10 +0800

    using document optimization

--- src/app/select-selection-demo/partial/using-documents/using-documents.component.ts
+++ src/app/select-selection-demo/partial/using-documents/using-documents.component.ts
@@ -1,15 +1,24 @@
 import { Component, OnInit } from '@angular/core';
 
 @Component({
-  selector: 'app-using-documents',
-  templateUrl: './using-documents.component.html',
-  styleUrls: ['./using-documents.component.scss']
+    selector: 'app-using-documents',
+    templateUrl: './using-documents.component.html',
+    styleUrls: ['./using-documents.component.scss']
 })
 export class UsingDocumentsComponent implements OnInit {
+    paramsList: Array<any> = [];
 
-  constructor() { }
+    constructor() { }
 
-  ngOnInit() {
-  }
-
-}
+    ngOnInit() {
+        this.paramsList = [
+            { name: 'selects', explain: '必选，数据源', type: 'Array<{text:string,value:string,disabled?:Boolean}>', optionaValue: '—', defaultValue: '-' },
+            { name: 'label', explain: '必选，label标签名', type: 'String', optionaValue: '—', defaultValue: '-' },
+            { name: 'showType', explain: '可选，显示类型（inline,block）', type: 'String', optionaValue: 'inline,block', defaultValue: 'inline' },
+            { name: 'selected', explain: '选择时向外传递事件方法，同时向外传递选择值,block）', type: '—', optionaValue: '—', defaultValue: '-' },
+            { name: 'defaultValue', explain: '可选，为组件内部传递默认值', type: 'string', optionaValue: '—', defaultValue: '-' },
+            { name: 'clearEventer', explain: '可选，配置清除功能时可用，点击清除时向外传递事件', type: '—', optionaValue: '—', defaultValue: '-' },
+            { name: 'disabled', explain: '当item项禁选时，数据对象添加‘disabled：true’字段即可', type: '—', optionaValue: '—', defaultValue: '-' }
+        ];
+    };
+};
--- src/app/vehicle-selection-demo/partial/using-documents/using-documents.component.ts
+++ src/app/vehicle-selection-demo/partial/using-documents/using-documents.component.ts
@@ -1,15 +1,18 @@
 import { Component, OnInit } from '@angular/core';
 
 @Component({
-  selector: 'app-using-documents',
-  templateUrl: './using-documents.component.html',
-  styleUrls: ['./using-documents.component.scss']
+    selector: 'app-using-documents',
+    templateUrl: './using-documents.component.html',
+    styleUrls: ['./using-documents.component.scss']
 })
 export class UsingDocumentsComponent implements OnInit {
+    paramsList: Array<any> = [];
 
-  constructor() { }
+    constructor() { }
 
-  ngOnInit() {
-  }
-
-}
+    ngOnInit() {
+        this.paramsList = [
+            { name: 'outputType', explain: '可选，选择输出类型', type: 'string', optionaValue: 'brand（车品牌），series（车系），model（车型）', defaultValue: 'model' }
+        ];
+    };
+};
