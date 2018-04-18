e7acf2667e7eb762d6d5e079e3ff600dff61c5e0
leihg <13589299411@163.com>
2018-04-18 13:19:46 +0800

    opt

--- src/app/order-management/partial/edit-order-modal/edit-order-modal.component.ts
+++ src/app/order-management/partial/edit-order-modal/edit-order-modal.component.ts
@@ -10,59 +10,62 @@ import { Logger } from '../../../core/logger.service';
 import { OrdersService } from '../../shared/orders.service';
 
 @Component({
-  selector: 'app-edit-order-modal',
-  templateUrl: './edit-order-modal.component.html',
-  styleUrls: ['./edit-order-modal.component.scss']
+    selector: 'app-edit-order-modal',
+    templateUrl: './edit-order-modal.component.html',
+    styleUrls: ['./edit-order-modal.component.scss']
 })
 export class EditOrderModalComponent implements OnInit {
 
-  log: Logger;
-  saving = false;
-  form: FormGroup;
-  @Input() data: any = {};
+    log: Logger;
+    saving = false;
+    form: FormGroup;
+    @Input() data: any = {};
 
-  hearFromItems: Array<any> = [
-    { key: 1, text: '广告' },
-    { key: 2, text: '传单' },
-    { key: 3, text: '朋友' }
-  ];
+    hearFromItems: Array<any> = [
+        { key: 1, text: '广告' },
+        { key: 2, text: '传单' },
+        { key: 3, text: '朋友' }
+    ];
 
-  constructor(private ordersService: OrdersService,
-    private loggerFactory: LoggerFactory, private formBuilder: FormBuilder,
-    public activeModal: BsModalRef, private cdf: ChangeDetectorRef) {
-    this.log = this.loggerFactory.getLogger('编辑订单');
-    this.buildForm();
-  }
+    constructor(
+        private ordersService: OrdersService,
+        private loggerFactory: LoggerFactory,
+        private formBuilder: FormBuilder,
+        public activeModal: BsModalRef,
+        private cdf: ChangeDetectorRef) {
+        this.log = this.loggerFactory.getLogger('编辑订单');
+        this.buildForm();
+    };
 
-  ngOnInit() {
-  }
+    ngOnInit() {
+    };
 
-  submit() {
-    if (!this.form.valid) {
-      this.log.error('表单数据不完整，请检查。');
-      return;
+    submit() {
+        if (!this.form.valid) {
+            this.log.error('表单数据不完整，请检查。');
+            return;
+        }
+
+        this.saving = true;
+        this.ordersService.updateOrder(this.data)
+            .subscribe(response => {
+                this.saving = false;
+                this.log.info('成功修改订单!', response);
+                this.activeModal.hide();
+            }, error => {
+                this.saving = false;
+                this.log.error('订单保存失败。', error);
+            });
     }
 
-    this.saving = true;
-    this.ordersService.updateOrder(this.data)
-      .subscribe(response => {
-        this.saving = false;
-        this.log.info('成功修改订单!', response);
-        this.activeModal.hide();
-      }, error => {
-        this.saving = false;
-        this.log.error('订单保存失败。', error);
-      });
-  }
-
-  private buildForm() {
-    this.form = this.formBuilder.group({
-      subject: [null, [Validators.required, Validators.maxLength(50)]],
-      date: [null, [Validators.required]],
-      hearFrom: [null, [Validators.required]],
-      price: [null, [Validators.required]],
-      amount: [null, [Validators.required]]
-    });
-  }
+    private buildForm() {
+        this.form = this.formBuilder.group({
+            subject: [null, [Validators.required, Validators.maxLength(50)]],
+            date: [null, [Validators.required]],
+            hearFrom: [null, [Validators.required]],
+            price: [null, [Validators.required]],
+            amount: [null, [Validators.required]]
+        });
+    }
 
 }
