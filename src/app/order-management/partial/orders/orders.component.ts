44872bf214d3bc30bd515f7e66ec88eaf628aa55
leihg <13589299411@163.com>
2018-10-25 10:12:52 +0800

    添加列表简洁页脚代码注释

--- src/app/order-management/partial/orders/orders.component.ts
+++ src/app/order-management/partial/orders/orders.component.ts
@@ -78,7 +78,7 @@ export class OrdersComponent implements OnInit, AfterViewInit {
             .finally(() => this.loading = false)
             .subscribe(response => {
                 this.datatable.count = response.count;
-                // 使用简洁翻页组件时
+                // 使用简洁翻页组件时，用data的长度赋值给datatable.count
                 // this.datatable.count = response.data.length;
                 this.orders = response.data;
                 this.log.debug('订单列表', this.orders);
