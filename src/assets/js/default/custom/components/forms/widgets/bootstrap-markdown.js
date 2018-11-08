93bb821641a9e6cb0b197db4ac544b41e2885965
leihg <13589299411@163.com>
2018-11-08 18:06:49 +0800

    添加框架静态资源（图标&图片）

---
+++ src/assets/js/default/custom/components/forms/widgets/bootstrap-markdown.js
@@ -0,0 +1,18 @@
+//== Class definition
+
+var BootstrapMarkdown = (function() {
+  //== Private functions
+  var demos = function() {};
+
+  return {
+    // public functions
+    init: function() {
+      demos();
+    }
+  };
+})();
+
+//== Initialization
+jQuery(document).ready(function() {
+  BootstrapMarkdown.init();
+});
---
+++ src/assets/js/default/custom/components/maps/jvectormap.js
@@ -0,0 +1,18 @@
+//== Class definition
+var jVectorMap = (function() {
+  //== Private functions
+
+  var demo1 = function() {};
+
+  return {
+    // public functions
+    init: function() {
+      // default charts
+      demo1();
+    }
+  };
+})();
+
+jQuery(document).ready(function() {
+  jVectorMap.init();
+});
