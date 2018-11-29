ae8608dee32cafcaf285074506cd559d6b43f278
leihg <13589299411@163.com>
2018-11-29 15:30:52 +0800

    添加home模块

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,6 +1,11 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+
 import { finalize } from 'rxjs/operators';
 
+import { Logger } from '../core/logger.service';
+import { LoggerFactory } from '../core/logger-factory.service';
+import { ProfileService } from '../core/profile/profile.service';
 
 @Component({
   selector: 'app-home',
@@ -8,14 +13,48 @@ import { finalize } from 'rxjs/operators';
   styleUrls: ['./home.component.scss']
 })
 export class HomeComponent implements OnInit {
+  log: Logger;
+  menuLists: Array<any> = [];
 
-  quote: string;
-  isLoading: boolean;
-
-  constructor() { }
-
-  ngOnInit() {
-    this.isLoading = true;
+  constructor(
+    private router: Router,
+    private profileService: ProfileService,
+    private loggerFactory: LoggerFactory) {
+    this.log = this.loggerFactory.getLogger(`Home`);
   }
 
+  ngOnInit() {
+    this.getMenuItems();
+  }
+
+  getMenuItems(): void {
+    this.profileService.getMenuItems()
+      .subscribe(menuItems => {
+        this.menuLists = this.arrangementMenu(menuItems);
+      }, error => {
+        this.log.error(`组件库数据获取失败！`);
+      });
+  }
+
+  arrangementMenu(menuList: Array<any>) {
+    const menu: Array<any> = [];
+    menuList.forEach((item: any) => {
+      if (item.ngUrl !== undefined && item.children.length === 0) {
+        menu.push({
+          name: item.name,
+          icon: item.icon,
+          url: item.ngUrl,
+        });
+      } else {
+        item.children.forEach((itemt: any) => {
+          menu.push({
+            name: itemt.name,
+            icon: itemt.icon,
+            url: itemt.ngUrl,
+          });
+        });
+      }
+    });
+    return menu;
+  }
 }
