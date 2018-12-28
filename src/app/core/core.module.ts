69675fe98362602f08ce199f9ded10fc6094ea8c
leihg <13589299411@163.com>
2018-12-28 10:40:15 +0800

    lint code

--- src/app/core/core.module.ts
+++ src/app/core/core.module.ts
@@ -5,19 +5,16 @@ import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { HttpClient, HttpClientModule } from '@angular/common/http';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
-import { TranslateModule } from '@ngx-translate/core';
-import { ModalModule } from 'ngx-bootstrap';
-import { defineLocale } from 'ngx-bootstrap/chronos';
-import { zhCnLocale } from 'ngx-bootstrap/locale';
-import { ToastrModule } from 'ngx-toastr';
-import { MomentModule } from 'ngx-moment';
-import { TreeModule } from 'angular-tree-component';
-
-import { SharedModule } from '@app/shared/shared.module';
-import { SweetAlert2Service } from '@app/shared/dialogs/sweetalert2.service';
 import { SsoModule } from '../sso/sso.module';
-import { SsoServiceService } from '../sso/shared/sso-service.service';
+import { zhCnLocale } from 'ngx-bootstrap/locale';
+import { TreeModule } from 'angular-tree-component';
+import { defineLocale } from 'ngx-bootstrap/chronos';
+import { TranslateModule } from '@ngx-translate/core';
+import { SharedModule } from '@app/shared/shared.module';
+
 import { ProfileService } from './profile/profile.service';
+import { SsoServiceService } from '../sso/shared/sso-service.service';
+import { SweetAlert2Service } from '@app/shared/dialogs/sweetalert2.service';
 
 import {
   CoreRoutingModule, AuthenticationService, AuthenticationOAuth2Service,
@@ -35,63 +32,58 @@ defineLocale('zh-cn', zhCnLocale);
 
 @NgModule({
   imports: [
-    CommonModule,
-    FormsModule,
-    ReactiveFormsModule,
-    HttpClientModule,
-    TranslateModule,
-    RouterModule,
-    BrowserAnimationsModule,
-    ModalModule.forRoot(),
-    CoreRoutingModule,
     SsoModule,
-    ToastrModule,
-    MomentModule,
-    TreeModule.forRoot(),
-    SharedModule
+    FormsModule,
+    RouterModule,
+    SharedModule,
+    CommonModule,
+    TranslateModule,
+    HttpClientModule,
+    CoreRoutingModule,
+    ReactiveFormsModule,
+    BrowserAnimationsModule,
+    TreeModule.forRoot()
   ],
   declarations: [
+    AppsComponent,
+    TabsComponent,
     ShellComponent,
     HeaderComponent,
-    NavigationComponent,
     FooterComponent,
-    PageHeaderComponent,
-    ChangePasswordModalComponent,
-    MenuItemComponent,
-    BreadcrumbsComponent,
-    SsoRegisterComponent,
     ProfileComponent,
-    QuickActionsComponent,
-    NotificationsComponent,
-    QuickSearchComponent,
     TopMenuComponent,
     ActionsComponent,
     ReportsComponent,
-    AppsComponent,
-    TabsComponent,
+    MenuItemComponent,
+    NavigationComponent,
+    PageHeaderComponent,
+    BreadcrumbsComponent,
+    SsoRegisterComponent,
+    QuickSearchComponent,
+    QuickActionsComponent,
+    NotificationsComponent,
+    ChangePasswordModalComponent,
     InitMenutCloseDirective
   ],
-  entryComponents: [
-    ChangePasswordModalComponent
-  ],
+  entryComponents: [ChangePasswordModalComponent],
   providers: [
-    AuthenticationService,
-    AuthenticationOAuth2Service,
-    AuthenticationGuard,
+    Dialogs,
     I18nService,
+    LoggerFactory,
+    ProfileService,
     HttpCacheService,
-    ApiPrefixInterceptor,
-    ErrorHandlerInterceptor,
     CacheInterceptor,
+    SsoServiceService,
+    SweetAlert2Service,
+    AuthenticationGuard,
+    ApiPrefixInterceptor,
+    AuthenticationService,
+    ErrorHandlerInterceptor,
+    AuthenticationOAuth2Service,
     {
       provide: HttpClient,
       useClass: HttpService
     },
-    Dialogs,
-    SweetAlert2Service,
-    SsoServiceService,
-    ProfileService,
-    LoggerFactory,
   ]
 })
 export class CoreModule {
